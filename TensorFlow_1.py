09edd2344fdea1b9cf39605c1daec9308b55fd5c
KEUMGYEUNGSIK <101696085+KEUMGYEUNGSIK@users.noreply.github.com>
2022-03-17 11:02:28 +0900

    Create TensorFlow_1.py

---
+++ TensorFlow_1.py
@@ -0,0 +1,34 @@
+#1
+print('Hello world')
+#2
+print("Mary's cosmetics")
+#3
+print("신씨가 소리질렀다. '도둑이야'.")
+#4
+print('"C:\Windows"')
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#51
+movie_rank = ['닥터스트레인지', '스플릿', '럭키']
+#52
+movie_rank.append("배트맨")
+#53
+movie_rank.insert(1,"슈퍼맨")
+#58
+nums = [1, 2, 3, 4, 5]
+sum(nums)
+#59
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+#60
+nums = [1, 2, 3, 4, 5]
+avg = sum(nums)/5
+#102
+print( 3 == 5)
+#103
+print( 3 < 5 )
+#104
+x = 4
+print( 1 < x < 5 )
+#105
+print((3 == 3) and (4 != 3))
